1db10e7d73fc01e1673f2bdd2e0b85a0d5e0f9ef
Martin Breining <martin.breining@gmail.com>
2012-09-24 21:19:14 -0700

    Load evernote gem

--- ruby/evernote-vim/controller.rb
+++ ruby/evernote-vim/controller.rb
@@ -6,10 +6,7 @@ require "thrift/protocol/base_protocol"
 require "thrift/protocol/binary_protocol"
 require "thrift/transport/base_transport"
 require "thrift/transport/http_client_transport"
-require "Evernote/EDAM/user_store"
-require "Evernote/EDAM/user_store_constants.rb"
-require "Evernote/EDAM/note_store"
-require "Evernote/EDAM/limits_constants.rb"
+require "evernote"
 
 module EvernoteVim
   class Controller
